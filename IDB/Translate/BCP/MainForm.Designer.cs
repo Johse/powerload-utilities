bc467216ed0240590a6fdff9e21094a1bf3141b4
wangdu <wangdu.gyalpo@coolorange.com>
2021-04-20 16:43:04 +0200

    added back the changes which got lost during the git auto merge

--- IDB/Translate/BCP/MainForm.Designer.cs
+++ IDB/Translate/BCP/MainForm.Designer.cs
@@ -50,7 +50,7 @@
 			// 
 			this.label1.AutoSize = true;
 			this.label1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.Location = new System.Drawing.Point(14, 22);
+			this.label1.Location = new System.Drawing.Point(12, 19);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(174, 13);
 			this.label1.TabIndex = 0;
@@ -61,9 +61,9 @@
 			this.txtConnectionString.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.txtConnectionString.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.txtConnectionString.Location = new System.Drawing.Point(17, 40);
+			this.txtConnectionString.Location = new System.Drawing.Point(15, 35);
 			this.txtConnectionString.Name = "txtConnectionString";
-			this.txtConnectionString.Size = new System.Drawing.Size(462, 22);
+			this.txtConnectionString.Size = new System.Drawing.Size(397, 22);
 			this.txtConnectionString.TabIndex = 1;
 			this.txtConnectionString.Text = "Server=(local)\\AUTODESKVAULT;Database=Load;Trusted_Connection=True;";
 			// 
@@ -72,9 +72,9 @@
 			this.btnExport.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.btnExport.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.btnExport.Location = new System.Drawing.Point(17, 249);
+			this.btnExport.Location = new System.Drawing.Point(15, 216);
 			this.btnExport.Name = "btnExport";
-			this.btnExport.Size = new System.Drawing.Size(463, 27);
+			this.btnExport.Size = new System.Drawing.Size(397, 23);
 			this.btnExport.TabIndex = 6;
 			this.btnExport.Text = "Create BCP Package";
 			this.btnExport.UseVisualStyleBackColor = true;
@@ -85,9 +85,9 @@
 			this.txtExportDirectory.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.txtExportDirectory.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.txtExportDirectory.Location = new System.Drawing.Point(17, 135);
+			this.txtExportDirectory.Location = new System.Drawing.Point(15, 117);
 			this.txtExportDirectory.Name = "txtExportDirectory";
-			this.txtExportDirectory.Size = new System.Drawing.Size(409, 22);
+			this.txtExportDirectory.Size = new System.Drawing.Size(351, 22);
 			this.txtExportDirectory.TabIndex = 3;
 			this.txtExportDirectory.Text = "C:\\TEMP\\BCP Package Directory";
 			// 
@@ -95,9 +95,9 @@
 			// 
 			this.btnExportDirectory.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
 			this.btnExportDirectory.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.btnExportDirectory.Location = new System.Drawing.Point(434, 135);
+			this.btnExportDirectory.Location = new System.Drawing.Point(372, 117);
 			this.btnExportDirectory.Name = "btnExportDirectory";
-			this.btnExportDirectory.Size = new System.Drawing.Size(47, 25);
+			this.btnExportDirectory.Size = new System.Drawing.Size(40, 22);
 			this.btnExportDirectory.TabIndex = 4;
 			this.btnExportDirectory.Text = "...";
 			this.btnExportDirectory.UseVisualStyleBackColor = true;
@@ -108,9 +108,9 @@
 			this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
 			this.btnClose.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.btnClose.Location = new System.Drawing.Point(376, 393);
+			this.btnClose.Location = new System.Drawing.Point(322, 341);
 			this.btnClose.Name = "btnClose";
-			this.btnClose.Size = new System.Drawing.Size(105, 27);
+			this.btnClose.Size = new System.Drawing.Size(90, 23);
 			this.btnClose.TabIndex = 7;
 			this.btnClose.Text = "Close";
 			this.btnClose.UseVisualStyleBackColor = true;
@@ -121,9 +121,9 @@
 			this.btnValidate.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.btnValidate.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.btnValidate.Location = new System.Drawing.Point(15, 177);
+			this.btnValidate.Location = new System.Drawing.Point(13, 153);
 			this.btnValidate.Name = "btnValidate";
-			this.btnValidate.Size = new System.Drawing.Size(463, 27);
+			this.btnValidate.Size = new System.Drawing.Size(397, 23);
 			this.btnValidate.TabIndex = 5;
 			this.btnValidate.Text = "Validate Database";
 			this.btnValidate.UseVisualStyleBackColor = true;
@@ -133,7 +133,7 @@
 			// 
 			this.label2.AutoSize = true;
 			this.label2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label2.Location = new System.Drawing.Point(14, 117);
+			this.label2.Location = new System.Drawing.Point(12, 101);
 			this.label2.Name = "label2";
 			this.label2.Size = new System.Drawing.Size(111, 13);
 			this.label2.TabIndex = 2;
@@ -142,9 +142,9 @@
 			// label3
 			// 
 			this.label3.AutoSize = true;
-			this.label3.Location = new System.Drawing.Point(14, 69);
+			this.label3.Location = new System.Drawing.Point(12, 60);
 			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(78, 15);
+			this.label3.Size = new System.Drawing.Size(69, 13);
 			this.label3.TabIndex = 8;
 			this.label3.Text = "Vault Version";
 			// 
@@ -156,9 +156,9 @@
             "2019",
             "2018",
             "2017"});
-			this.comboBoxVaultVersion.Location = new System.Drawing.Point(17, 89);
+			this.comboBoxVaultVersion.Location = new System.Drawing.Point(15, 77);
 			this.comboBoxVaultVersion.Name = "comboBoxVaultVersion";
-			this.comboBoxVaultVersion.Size = new System.Drawing.Size(462, 23);
+			this.comboBoxVaultVersion.Size = new System.Drawing.Size(397, 21);
 			this.comboBoxVaultVersion.TabIndex = 9;
 			this.comboBoxVaultVersion.Text = "2020";
 			// 
@@ -166,9 +166,9 @@
 			// 
 			this.progressBarTask.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.progressBarTask.Location = new System.Drawing.Point(17, 336);
+			this.progressBarTask.Location = new System.Drawing.Point(15, 291);
 			this.progressBarTask.Name = "progressBarTask";
-			this.progressBarTask.Size = new System.Drawing.Size(463, 27);
+			this.progressBarTask.Size = new System.Drawing.Size(397, 23);
 			this.progressBarTask.Step = 1;
 			this.progressBarTask.TabIndex = 10;
 			// 
@@ -177,48 +177,49 @@
 			this.labelProgressTask.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.labelProgressTask.AutoSize = true;
-			this.labelProgressTask.Location = new System.Drawing.Point(14, 366);
+			this.labelProgressTask.Location = new System.Drawing.Point(12, 317);
 			this.labelProgressTask.Name = "labelProgressTask";
-			this.labelProgressTask.Size = new System.Drawing.Size(0, 15);
+			this.labelProgressTask.Size = new System.Drawing.Size(0, 13);
 			this.labelProgressTask.TabIndex = 11;
 			// 
 			// progressBarTotal
 			// 
 			this.progressBarTotal.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.progressBarTotal.Location = new System.Drawing.Point(17, 301);
+			this.progressBarTotal.Location = new System.Drawing.Point(15, 261);
 			this.progressBarTotal.Name = "progressBarTotal";
-			this.progressBarTotal.Size = new System.Drawing.Size(463, 27);
+			this.progressBarTotal.Size = new System.Drawing.Size(397, 23);
 			this.progressBarTotal.Step = 1;
 			this.progressBarTotal.TabIndex = 12;
 			// 
 			// labelProgressTotal
 			// 
 			this.labelProgressTotal.AutoSize = true;
-			this.labelProgressTotal.Location = new System.Drawing.Point(17, 284);
+			this.labelProgressTotal.Location = new System.Drawing.Point(15, 246);
 			this.labelProgressTotal.Name = "labelProgressTotal";
-			this.labelProgressTotal.Size = new System.Drawing.Size(89, 15);
+			this.labelProgressTotal.Size = new System.Drawing.Size(78, 13);
 			this.labelProgressTotal.TabIndex = 13;
 			this.labelProgressTotal.Text = "Progress Total:";
 			// 
 			// DisableConfigurationExportCheckBox
 			// 
 			this.DisableConfigurationExportCheckBox.AutoSize = true;
-			this.DisableConfigurationExportCheckBox.Enabled = false;
-			this.DisableConfigurationExportCheckBox.Location = new System.Drawing.Point(20, 225);
+			this.DisableConfigurationExportCheckBox.Checked = true;
+			this.DisableConfigurationExportCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.DisableConfigurationExportCheckBox.Location = new System.Drawing.Point(17, 195);
 			this.DisableConfigurationExportCheckBox.Margin = new System.Windows.Forms.Padding(2);
 			this.DisableConfigurationExportCheckBox.Name = "DisableConfigurationExportCheckBox";
-			this.DisableConfigurationExportCheckBox.Size = new System.Drawing.Size(182, 19);
+			this.DisableConfigurationExportCheckBox.Size = new System.Drawing.Size(159, 17);
 			this.DisableConfigurationExportCheckBox.TabIndex = 14;
 			this.DisableConfigurationExportCheckBox.Text = "Disable Configuration Export";
 			this.DisableConfigurationExportCheckBox.UseVisualStyleBackColor = true;
 			// 
 			// MainForm
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.CancelButton = this.btnClose;
-			this.ClientSize = new System.Drawing.Size(511, 434);
+			this.ClientSize = new System.Drawing.Size(438, 376);
 			this.Controls.Add(this.DisableConfigurationExportCheckBox);
 			this.Controls.Add(this.labelProgressTotal);
 			this.Controls.Add(this.progressBarTotal);
@@ -234,9 +235,7 @@
 			this.Controls.Add(this.btnExport);
 			this.Controls.Add(this.txtConnectionString);
 			this.Controls.Add(this.label1);
-			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-			this.MaximizeBox = false;
 			this.Name = "MainForm";
 			this.Text = "IDB.Translate.BCP";
 			this.ResumeLayout(false);
